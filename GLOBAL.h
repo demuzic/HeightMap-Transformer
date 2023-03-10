e6843e8c50cc281374a87375611104efdc41a434
Murilo Castro <57036399+demuzic@users.noreply.github.com>
2023-03-10 16:35:14 -0300

    Add files via upload

---
+++ GLOBAL.h
@@ -0,0 +1,6 @@
+#ifndef GLOBAL_H_INCLUDED
+#define GLOBAL_H_INCLUDED
+
+int editor();
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "raylib.h"
+#include <stdlib.h>
+#include <cstdio>
+#include <iostream>
+#include <raymath.h>
+#include <string>
+#include "GLOBAL.h"
+
+int main()
+{
+	editor();
+	return 0;
+}
